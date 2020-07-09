f158fd86b32e1d450073d90069b616ddb879a117
MacMcClain <macmcclain.dev@gmail.com>
2020-07-08 23:59:07 -0700

    added support for parms on nav

--- src/lib/navigate.js
+++ src/lib/navigate.js
@@ -1,7 +1,14 @@
+const queryString = require('query-string');
 
-
-const toApp = (app) => {
+const toApp = (app, params) => {
   const mode = process.env.NODE_ENV;
+
+  let url = app;
+  if(params) {
+    const qs = queryString.stringify(params);
+    url += "#?" + qs;
+  }
+
   if(mode === "development" ) {
     alert(`Simulating navigation to '${app}' while in development environment. `)
   }
